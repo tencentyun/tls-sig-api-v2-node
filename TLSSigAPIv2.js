6a78ce103432602031a9c9cf0a414ec36c0fe30d
yutingzeng <yutingzeng@tencent.com>
2020-08-06 16:15:58 +0800

    代码格式化

--- TLSSigAPIv2.js
+++ TLSSigAPIv2.js
@@ -20,42 +20,62 @@ base64url.escape = function escape(str) {
         .replace(/=/g, '_');
 };
 
-base64url.encode = function encode (str) {
+base64url.encode = function encode(str) {
     return this.escape(newBuffer(str).toString('base64'));
 };
 
-base64url.decode = function decode (str) {
+base64url.decode = function decode(str) {
     return newBuffer(this.unescape(str), 'base64').toString();
 };
 
 function base64encode(str) {
     return newBuffer(str).toString('base64')
 }
+
 function base64decode(str) {
     return newBuffer(str, 'base64').toString()
 }
 
-var Api = function(sdkappid, key) {
-    this.sdkappid  = sdkappid;
+var Api = function (sdkappid, key) {
+    this.sdkappid = sdkappid;
     this.key = key;
 };
-/**用于生成实时音视频(TRTC)业务进房权限加密串,具体用途用法参考TRTC文档：https://cloud.tencent.com/document/product/647/32240 
-* TRTC业务进房权限加密串需使用用户定义的userbuf
-* @brief 生成 userbuf
-* @param account 用户名
-* @param dwSdkappid sdkappid
-* @param dwAuthID  数字房间号
-* @param dwExpTime 过期时间：该权限加密串的过期时间，建议300秒，实际过期时间:now+dwExpTime
-* @param dwPrivilegeMap 用户权限，255表示所有权限
-* @param dwAccountType 用户类型,默认为0
-* @return userbuf  {string}  返回的userbuf
-*/
+/**
+ * 通过传入参数生成 base64 的 hmac 值
+ * @param identifier
+ * @param currTime
+ * @param expire
+ * @returns {string}
+ * @private
+ */
+Api.prototype._hmacsha256 = function (identifier, currTime, expire, base64UserBuf) {
+    var contentToBeSigned = "TLS.identifier:" + identifier + "\n";
+    contentToBeSigned += "TLS.sdkappid:" + this.sdkappid + "\n";
+    contentToBeSigned += "TLS.time:" + currTime + "\n";
+    contentToBeSigned += "TLS.expire:" + expire + "\n";
+    if (null != base64UserBuf) {
+        contentToBeSigned += "TLS.userbuf:" + base64UserBuf + "\n";
+    }
+    const hmac = crypto.createHmac("sha256", this.key);
+    return hmac.update(contentToBeSigned).digest('base64');
+};
+/**
+ * TRTC业务进房权限加密串需使用用户定义的userbuf
+ * @brief 生成 userbuf
+ * @param account 用户名
+ * @param dwSdkappid sdkappid
+ * @param dwAuthID  数字房间号
+ * @param dwExpTime 过期时间：该权限加密串的过期时间，建议300秒，实际过期时间:now+dwExpTime
+ * @param dwPrivilegeMap 用户权限，255表示所有权限
+ * @param dwAccountType 用户类型,默认为0
+ * @return userbuf  {string}  返回的userbuf
+ */
 Api.prototype._genUserbuf = function (account, dwAuthID, dwExpTime,
-    dwPrivilegeMap, dwAccountType){
+    dwPrivilegeMap, dwAccountType) {
 
     let accountLength = account.length;
     let offset = 0;
-    let userBuf = new Buffer.alloc(1+2+accountLength+4+4+4+4+4);
+    let userBuf = new Buffer.alloc(1 + 2 + accountLength + 4 + 4 + 4 + 4 + 4);
 
     //cVer
     userBuf[offset++] = 0;
@@ -63,7 +83,7 @@ Api.prototype._genUserbuf = function (account, dwAuthID, dwExpTime,
     //wAccountLen
     userBuf[offset++] = (accountLength & 0xFF00) >> 8;
     userBuf[offset++] = accountLength & 0x00FF;
-    
+
     //buffAccount
     for (; offset < 3 + accountLength; ++offset) {
         userBuf[offset] = account.charCodeAt(offset - 3);
@@ -74,26 +94,26 @@ Api.prototype._genUserbuf = function (account, dwAuthID, dwExpTime,
     userBuf[offset++] = (this.sdkappid & 0x00FF0000) >> 16;
     userBuf[offset++] = (this.sdkappid & 0x0000FF00) >> 8;
     userBuf[offset++] = this.sdkappid & 0x000000FF;
-    
+
     //dwAuthId
     userBuf[offset++] = (dwAuthID & 0xFF000000) >> 24;
     userBuf[offset++] = (dwAuthID & 0x00FF0000) >> 16;
     userBuf[offset++] = (dwAuthID & 0x0000FF00) >> 8;
     userBuf[offset++] = dwAuthID & 0x000000FF;
-    
+
     //过期时间：dwExpTime+now
-    let expire = Date.now()/1000 +dwExpTime;
+    let expire = Date.now() / 1000 + dwExpTime;
     userBuf[offset++] = (expire & 0xFF000000) >> 24;
     userBuf[offset++] = (expire & 0x00FF0000) >> 16;
     userBuf[offset++] = (expire & 0x0000FF00) >> 8;
     userBuf[offset++] = expire & 0x000000FF;
-    
+
     //dwPrivilegeMap
     userBuf[offset++] = (dwPrivilegeMap & 0xFF000000) >> 24;
     userBuf[offset++] = (dwPrivilegeMap & 0x00FF0000) >> 16;
     userBuf[offset++] = (dwPrivilegeMap & 0x0000FF00) >> 8;
     userBuf[offset++] = dwPrivilegeMap & 0x000000FF;
-    
+
     //dwAccountType
     userBuf[offset++] = (dwAccountType & 0xFF000000) >> 24;
     userBuf[offset++] = (dwAccountType & 0x00FF0000) >> 16;
@@ -103,37 +123,15 @@ Api.prototype._genUserbuf = function (account, dwAuthID, dwExpTime,
     return userBuf;
 }
 /**
- * 通过传入参数生成 base64 的 hmac 值
- * @param identifier
- * @param currTime
- * @param expire
- * @returns {string}
- * @private
- */
-Api.prototype._hmacsha256 = function(identifier, currTime, expire, base64UserBuf){
-    var contentToBeSigned = "TLS.identifier:" + identifier + "\n";
-    contentToBeSigned += "TLS.sdkappid:"+ this.sdkappid + "\n";
-    contentToBeSigned += "TLS.time:" + currTime + "\n";
-    contentToBeSigned += "TLS.expire:" + expire + "\n";
-    if (null != base64UserBuf) {
-        contentToBeSigned += "TLS.userbuf:" + base64UserBuf + "\n";
-    }
-    const hmac = crypto.createHmac("sha256", this.key);
-    return hmac.update(contentToBeSigned).digest('base64');
-};
-
-/**
  *【功能说明】用于签发 TRTC 和 IM 服务中必须要使用的 UserSig 鉴权票据
  *
  *【参数说明】
- * userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
- * expire - UserSig 票据的过期时间，单位是秒，比如 86400 代表生成的 UserSig 票据在一天后就无法再使用了。
+ * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
+ * @param expire - UserSig 票据的过期时间，单位是秒，比如 86400 代表生成的 UserSig 票据在一天后就无法再使用了。
  */
-Api.prototype.genUserSig = function(userid, expire){
-    return this.genPrivateMapKey(userid, expire, null,null);
+Api.prototype.genUserSig = function (userid, expire) {
+    return this.genPrivateMapKey(userid, expire, null, null);
 };
-
-
 
 /**
  *【功能说明】
@@ -144,10 +142,10 @@ Api.prototype.genUserSig = function(userid, expire){
  * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】=>【应用管理】=>【应用信息】中打开“启动权限密钥”开关。
  *
  *【参数说明】
- * userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
- * expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
- * roomid - 房间号，用于指定该 userid 可以进入的房间号
- * privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
+ * @param userid - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
+ * @param expire - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
+ * @param roomid - 房间号，用于指定该 userid 可以进入的房间号
+ * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
  *  - 第 1 位：0000 0001 = 1，创建房间的权限
  *  - 第 2 位：0000 0010 = 2，加入房间的权限
  *  - 第 3 位：0000 0100 = 4，发送语音的权限
@@ -159,17 +157,17 @@ Api.prototype.genUserSig = function(userid, expire){
  *  - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
  *  - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
  */
-Api.prototype.genPrivateMapKey = function(userid, expire, roomid,privilegeMap){
+Api.prototype.genPrivateMapKey = function (userid, expire, roomid, privilegeMap) {
 
-    if(null == roomid || null == privilegeMap )
-        var userBuf = null ;
+    if (null == roomid || null == privilegeMap)
+        var userBuf = null;
     else
-        var userBuf = this._genUserbuf(userid,roomid,expire,privilegeMap,0);
-    var currTime = Math.floor(Date.now()/1000);
+        var userBuf = this._genUserbuf(userid, roomid, expire, privilegeMap, 0);
+    var currTime = Math.floor(Date.now() / 1000);
 
     var sigDoc = {
         'TLS.ver': "2.0",
-        'TLS.identifier': ""+userid,
+        'TLS.identifier': "" + userid,
         'TLS.sdkappid': Number(this.sdkappid),
         'TLS.time': Number(currTime),
         'TLS.expire': Number(expire)
